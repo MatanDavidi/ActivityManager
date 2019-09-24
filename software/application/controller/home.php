af42eca74e340062b01b6a5fa232dffd7242ec35
Matan Davidi <davidi.matan@gmail.com>
2019-09-24 16:22:58 +0200

    Add login method and GET vs POST logic to show the login page or to attempt to login the collaborator

--- software/application/controller/home.php
+++ software/application/controller/home.php
@@ -7,11 +7,26 @@ class Home
     public function index()
     {
 
-        require "application/views/shared/header.php";
         require "application/views/home/index.php";
         require "application/views/shared/footer.php";
 
     }
 
+    public function login()
+    {
+
+        if ($_SERVER["REQUEST_METHOD"] === "GET") {
+
+            require "application/views/shared/header.php";
+            require "application/views/home/login.php";
+            require "application/views/shared/footer.php";
+
+        } else {
+
+            echo "Login attempt";
+
+        }
+
+    }
 
 }
