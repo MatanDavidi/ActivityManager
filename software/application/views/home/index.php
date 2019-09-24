5f19116330c31ea4f989a6729c7a564cb39cdc1d
Matan Davidi <davidi.matan@gmail.com>
2019-09-24 14:02:29 +0200

    Add index view

--- software/application/views/home/index.php
+++ software/application/views/home/index.php
@@ -1,1 +1,17 @@
-<?php
+<!-- banner part start-->
+<section class="banner_part">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6 offset-lg-1">
+                <div class="banner_text">
+                    <div class="banner_text_iner">
+                        <h5>Gestione delle ore di lavoro</h5>
+                        <h1>ActivityManager</h1>
+                        <a href="#" class="btn_1">Esegui l'accesso</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- banner part start-->
