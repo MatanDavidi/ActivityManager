fbdb19132ddbe51e5220552d6b176a4228b98630
Matan Davidi <davidi.matan@gmail.com>
2019-09-24 14:22:49 +0200

    Update footer with personal information

--- software/application/views/shared/footer.php
+++ software/application/views/shared/footer.php
@@ -2,73 +2,26 @@
 <footer class="footer-area">
     <div class="container">
         <div class="row justify-content-between">
-            <div class="col-sm-6 col-md-3 col-xl-3">
+            <div class="col-sm-12 col-md-12 col-xl-12">
                 <div class="single-footer-widget footer_1">
                     <a href="index.html">
-                        <img src="application/libs//img/footer_logo.png" alt=""/>
+                        <h6>Creato da</h6>
+
                     </a>
                     <ul>
-                        <li><a href="#">Work</a></li>
-                        <li><a href="#">Services</a></li>
-                        <li><a href="#">Contact</a></li>
+                        <li><a>Matan Davidi</a></li>
+                        <li><a>Scuola Arti e Mestieri Trevano, I4AA - Progetti individuali</a></li>
+                        <li><a href="mailto:davidi.matan@gmail.com">davidi.matan@gmail.com</a></li>
                     </ul>
                     <div class="social_icon">
-                        <a href="#"><i class="ti-facebook"></i></a>
-                        <a href="#"><i class="ti-twitter-alt"></i></a>
-                        <a href="#"><i class="ti-dribbble"></i></a>
-                        <a href="#"><i class="ti-instagram"></i></a>
+                        <a href="https://github.com/MatanDavidi"><i class="ti-github"></i></a>
+
+
                     </div>
                 </div>
             </div>
-            <div class="col-xl-3 col-sm-6 col-md-3">
-                <div class="single-footer-widget footer_2">
-                    <h4>Company</h4>
-                    <ul>
-                        <li>
-                            <a href="#">Media</a>
-                        </li>
-                        <li>
-                            <a href="#">Carrier</a>
-                        </li>
-                        <li>
-                            <a href="#">Testimonials</a>
-                        </li>
-                        <li>
-                            <a href="#">FAQ</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-xl-3 col-sm-6 col-md-3">
-                <div class="single-footer-widget footer_2">
-                    <h4>Resources</h4>
-                    <ul>
-                        <li>
-                            <a href="#">UI kit</a>
-                        </li>
-                        <li>
-                            <a href="#">WordPress theme</a>
-                        </li>
-                        <li>
-                            <a href="#">Illustration</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-xl-3 col-sm-6 col-md-3">
-                <div class="single-footer-widget footer_2">
-                    <div class="single_contact_info">
-                        <h3>London - UK</h3>
-                        <p>127, Manchaster city, London</p>
-                        <p>+008 728 362 278</p>
-                    </div>
-                    <div class="single_contact_info">
-                        <h3>New York - USA</h3>
-                        <p>127, Manchaster city, London</p>
-                        <p>+008 728 362 278</p>
-                    </div>
-                </div>
-            </div>
+
+
         </div>
     </div>
     <div class="container-fluid">
@@ -79,7 +32,9 @@
                         <div class="col-lg-12">
                             <p class="footer-text m-0">
                                 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                                Copyright &copy;<script>document.write(new Date().getFullYear());</script>
+                                Copyright ©
+                                <script>document.write(new Date().getFullYear());</script>
+                                2019
                                 All rights reserved | This template is made with <i class="ti-heart"
                                                                                     aria-hidden="true"></i> by <a
                                         href="https://colorlib.com" target="_blank">Colorlib</a>
