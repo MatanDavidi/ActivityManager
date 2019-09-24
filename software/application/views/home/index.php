f820d3fa120c2ce09aae74fd4b61986316fc87df
Matan Davidi <davidi.matan@gmail.com>
2019-09-24 16:21:33 +0200

    Add index-specific header to index view

--- software/application/views/home/index.php
+++ software/application/views/home/index.php
@@ -1,3 +1,119 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8"/>
+    <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <title>ActivityManager - Gestisci le ore di lavoro della tua azienda in modo facile e veloce</title>
+
+    <!--    Thanks to icontree for the favicon     -->
+    <!--    https://www.iconfinder.com/icontree    -->
+    <!--    Also, thanks to https://realfavicongenerator.net/ for the favicon generation and implementation code    -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/application/libs/img/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/application/libs/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/application/libs/img/favicon-16x16.png">
+    <link rel="manifest" href="/application/libs/img/site.webmanifest">
+    <link rel="mask-icon" href="/application/libs/img/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="/application/libs/img/favicon.ico">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-config" content="/application/libs/img/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/application/libs/css/bootstrap.min.css"/>
+    <!-- animate CSS -->
+    <link rel="stylesheet" href="/application/libs/css/animate.css"/>
+    <!-- owl carousel CSS -->
+    <link rel="stylesheet" href="/application/libs/css/owl.carousel.min.css"/>
+    <!-- themify CSS -->
+    <link rel="stylesheet" href="/application/libs/css/themify-icons.css"/>
+    <!-- flaticon CSS -->
+    <link rel="stylesheet" href="/application/libs/css/flaticon.css"/>
+    <!-- font awesome CSS -->
+    <link rel="stylesheet" href="/application/libs/css/magnific-popup.css"/>
+    <!-- swiper CSS -->
+    <link rel="stylesheet" href="/application/libs/css/slick.css"/>
+    <!-- style CSS -->
+    <link rel="stylesheet" href="/application/libs/css/style.css"/>
+</head>
+
+<body>
+<!--::header part start::-->
+<header class="main_menu home_menu">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-12">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <a class="navbar-brand" href="<?php echo URL; ?>">
+                        <img src="/application/libs/img/logo.png" alt="logo"/>
+                    </a>
+                    <button
+                            class="navbar-toggler"
+                            type="button"
+                            data-toggle="collapse"
+                            data-target="#navbarSupportedContent"
+                            aria-controls="navbarSupportedContent"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                    >
+                        <span class="ti-menu"></span>
+                    </button>
+
+                    <div
+                            class="collapse navbar-collapse main-menu-item justify-content-center"
+                            id="navbarSupportedContent"
+                    >
+                        <ul class="navbar-nav align-items-center">
+                            <li class="nav-item">
+                                <a class="nav-link" href="<?php echo URL; ?>">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="about.html">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="services.html">services</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="blog.html">Blog</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a
+                                        class="nav-link dropdown-toggle"
+                                        href="blog.html"
+                                        id="navbarDropdown"
+                                        role="button"
+                                        data-toggle="dropdown"
+                                        aria-haspopup="true"
+                                        aria-expanded="false"
+                                >
+                                    Pages
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="project.html">project</a>
+                                    <a class="dropdown-item" href="single-blog.html"
+                                    >Single blog</a
+                                    >
+                                    <a class="dropdown-item" href="elements.html">Elements</a>
+                                </div>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="contact.html">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="social_icon d-none d-lg-block">
+                        <a href="https://github.com/MatanDavidi"><i class="ti-github"></i></a>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- Header part end-->
+
 <!-- banner part start-->
 <section class="banner_part">
     <div class="container">
@@ -7,7 +123,7 @@
                     <div class="banner_text_iner">
                         <h5>Gestione delle ore di lavoro</h5>
                         <h1>ActivityManager</h1>
-                        <a href="#" class="btn_1">Esegui l'accesso</a>
+                        <a href="<?php echo URL ?>home/login" class="btn_1">Esegui l'accesso</a>
                     </div>
                 </div>
             </div>
