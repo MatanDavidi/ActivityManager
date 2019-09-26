34439a375a265c0be4be38fd8a3dad76cc344ed0
Matan Davidi <davidi.matan@gmail.com>
2019-09-26 14:12:46 +0200

    Add index method that brings user to index page of "lavori" controller

--- software/application/controller/lavoriController.php
+++ software/application/controller/lavoriController.php
@@ -4,4 +4,11 @@
 class LavoriController
 {
 
+    public function index()
+    {
+        require "application/views/shared/header.php";
+        require "application/views/lavori/index.php";
+        require "application/views/shared/footer.php";
+    }
+
 }
