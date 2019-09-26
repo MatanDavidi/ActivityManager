b13782eb71f8181fa50f9cf21faacd100c6ef9ad
Matan Davidi <davidi.matan@gmail.com>
2019-09-26 16:19:22 +0200

    Add first draft of lavori/index page

--- software/application/views/lavori/index.php
+++ software/application/views/lavori/index.php
@@ -1,1 +1,42 @@
-<?php
+<!-- breadcrumb start-->
+<section class="breadcrumb breadcrumb_bg align-items-center">
+    <div class="container">
+        <div class="row align-items-center justify-content-between">
+            <div class="col-sm-6">
+                <div class="breadcrumb_tittle">
+                    <p>Mostra i lavori in corso</p>
+                    <h2>Lavori</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- breadcrumb end-->
+
+<div class="container mt-5 mb-4">
+    <div class="col-sm-12">
+        <a href="" class="col-sm-3 mb-2 btn btn-success">
+            <i class="ti-plus"></i> NUOVO LAVORO
+        </a>
+        <a href="" class="col-sm-3 mb-2 float-right btn btn-success">
+            <i class="ti-plus"></i> NUOVA RISORSA
+        </a>
+    </div>
+    <div class="table border border-dark rounded">
+        <div class="col">
+            <p>yea boi</p>
+        </div>
+        <div class="col">
+            <p>yea boi</p>
+        </div>
+        <div class="col">
+            <p>yea boi</p>
+        </div>
+        <div class="col">
+            <p>yea boi</p>
+        </div>
+        <div class="col">
+            <p>yea boi</p>
+        </div>
+    </div>
+</div>
