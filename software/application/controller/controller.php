1663834f2060f6933e43e62312bc9cae0f3218c9
Matan Davidi <davidi.matan@gmail.com>
2019-09-26 14:33:01 +0200

    Add method "redirect" to controller base class

--- software/application/controller/controller.php
+++ software/application/controller/controller.php
@@ -4,4 +4,27 @@
 class Controller
 {
 
+    /**
+     * Permette di redirezionare a un'azione di una classe controller da ovunque nel codice di un controller.
+     * @param string $controller Il nome del controller che deve gestire la richiesta.
+     * @param string $action Il nome dell'azione (metodo) al quale si vuole mandare.
+     * Questo parametro è opzionale e ha un valore di default di "index".
+     * @param array $parameters Eventuali parametri che si vuole passare all'azione, sotto forma di array.
+     * Questo parametro è opzionale e ha un valore di default di un array vuoto, che significa che non vengono passati parametri.
+     */
+    public function redirect(string $controller, string $action = "index", Array $parameters = Array())
+    {
+
+        //Costruisci l'URL della richiesta con l'URL di base dell'applicazione,
+        // il nome del controller, quello del metodo e gli argomenti.
+        $location = URL . "/" . $controller . "/" . $action . "/" . implode("/", $parameters);
+
+        //Manda il browser a quell'URL
+        header("Location: $location");
+        
+        //Ferma l'esecuzione del codice per evitare risultati inattesi
+        die();
+
+    }
+
 }
