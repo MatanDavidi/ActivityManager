5e9a9ac1c92d26c2c1d915bf0cf726fd534adf7f
Matan Davidi <davidi.matan@gmail.com>
2019-09-27 16:18:31 +0200

    Add require statements for index view

--- software/application/controller/assegnazioniController.php
+++ software/application/controller/assegnazioniController.php
@@ -5,4 +5,13 @@ require_once "application/controller/controller.php";
 class AssegnazioniController extends Controller
 {
 
+    public function index()
+    {
+
+        require "application/views/shared/header.php";
+        require "application/views/assegnazioni/index.php";
+        require "application/views/shared/footer.php";
+
+    }
+
 }
