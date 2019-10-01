ada58b4d3ac189f240edd81e3a7ee4c7d8c9f63d
Matan Davidi <davidi.matan@gmail.com>
2019-10-01 14:37:06 +0200

    Add assignment form

--- software/application/views/assegnazioni/index.php
+++ software/application/views/assegnazioni/index.php
@@ -14,4 +14,23 @@
 <!-- breadcrumb end-->
 
 <div class="container mt-5 mb-4">
+    <form action="<?php echo URL . "assegnazioni/assegna"; ?>" method="post">
+        <div>
+            <div class="form-group">
+                <label for="lavoro">Lavoro:</label>
+                <select class="wide disabled" name="lavoro" id="lavoro">
+                    <option value="Lavoro 1" selected>Lavoro 0</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="risorsa">Risorsa:</label>
+                <select class="wide" name="risorsa" id="risorsa">
+                    <option value=""></option>
+                    <option value="Roberto Gervasoni">Roberto Gervasoni</option>
+                    <option value="Franco Rezzonico">Franco Rezzonico</option>
+                </select>
+            </div>
+        </div>
+        <input class="btn btn-success mt-2 col-12" type="submit" value="ASSEGNA">
+    </form>
 </div>
