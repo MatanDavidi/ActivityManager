1984907b247c26c45a9ca8ed2e10024325959082
Matan Davidi <davidi.matan@gmail.com>
2019-10-01 14:43:20 +0200

    Add common base to view "oreLavoro/index"

--- software/application/views/oreLavoro/index.php
+++ software/application/views/oreLavoro/index.php
@@ -1,1 +1,17 @@
-<?php
+<!-- breadcrumb start-->
+<section class="breadcrumb breadcrumb_bg align-items-center">
+    <div class="container">
+        <div class="row align-items-center justify-content-between">
+            <div class="col-sm-6">
+                <div class="breadcrumb_tittle">
+                    <p>Mostra i lavori in corso</p>
+                    <h2>Lavori</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- breadcrumb end-->
+
+<div class="container mt-5 mb-4">
+</div>
