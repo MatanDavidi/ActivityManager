92487510d1e65e332c9c98f62a5bc617e620741e
Matan Davidi <davidi.matan@gmail.com>
2019-10-03 15:46:53 +0200

    Change button icon

--- software/application/views/lavori/index.php
+++ software/application/views/lavori/index.php
@@ -18,8 +18,8 @@
         <a href="<?php echo URL; ?>lavori/nuovo" class="col-sm-3 mb-2 btn btn-success">
             <i class="ti-plus"></i> NUOVO LAVORO
         </a>
-        <a href="<?php echo URL; ?>risorse" class="col-sm-3 mb-2 ml-auto btn btn-success">
-            <i class="ti-plus"></i> GESTISCI RISORSE
+        <a href="<?php echo URL; ?>risorse" class="col-sm-3 mb-2 ml-auto btn btn-danger">
+            <i class="ti-user"></i> GESTISCI RISORSE
         </a>
     </div>
     <div class="row border border-dark rounded pt-3 pb-3">
