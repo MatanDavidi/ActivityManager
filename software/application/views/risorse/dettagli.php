621fe4fc5ad0ce84cb024575208205a3ff8b26d7
Matan Davidi <davidi.matan@gmail.com>
2019-10-03 15:47:28 +0200

    Add resource's details page

--- software/application/views/risorse/dettagli.php
+++ software/application/views/risorse/dettagli.php
@@ -1,1 +1,51 @@
-<?php
+<!-- breadcrumb start-->
+<section class="breadcrumb breadcrumb_bg align-items-center">
+    <div class="container">
+        <div class="row align-items-center justify-content-between">
+            <div class="col-sm-6">
+                <div class="breadcrumb_tittle">
+                    <p>Mostra i dettagli della risorsa 'Roberto Gervasoni'</p>
+                    <h2>Dettagli</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- breadcrumb end-->
+
+<div class="container mt-5 mb-4">
+    <div class="row col-xs-12 col-lg-4 mb-1 ml-0">
+        <a href="<?php echo URL . "risorse"; ?>" class="btn btn-danger btn-block">
+            <i class="ti-arrow-left"></i> TORNA ALLA LISTA DELLE RISORSE
+        </a>
+    </div>
+    <div class="row border border-dark rounded p-3">
+        <div class="col-12">
+            <h1 class="text-center">Roberto Gervasoni</h1>
+        </div>
+        <div class="col-12">
+            <p class="text-info">Costo all'ora: xxx.xx</p>
+            <p class="text-info">Ruolo: utente</p>
+            <p class="text-info">Numero di lavori assegnati: xx</p>
+        </div>
+        <ul class="list-group col-12 mt-2">
+            <li class="list-group-item">
+                <p>Attività assegnate:</p>
+            </li>
+            <a href="<?php echo URL; ?>lavori/dettagli/lavoro 1">
+                <li class="list-group-item list-group-item-action">
+                    Lavoro 1
+                </li>
+            </a>
+            <a href="<?php echo URL; ?>lavori/dettagli/lavoro 2">
+                <li class="list-group-item list-group-item-action">
+                    Lavoro 2
+                </li>
+            </a>
+            <a href="<?php echo URL; ?>lavori/dettagli/lavoro 3">
+                <li class="list-group-item list-group-item-action">
+                    Lavoro 3
+                </li>
+            </a>
+        </ul>
+    </div>
