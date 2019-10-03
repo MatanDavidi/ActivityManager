a5e706243b760bac263a03075aac517d82e4fef6
Matan Davidi <davidi.matan@gmail.com>
2019-10-03 15:11:15 +0200

    Add redirection to view "Risorse/index"

--- software/application/controller/risorseController.php
+++ software/application/controller/risorseController.php
@@ -7,8 +7,10 @@ class RisorseController
     public function index()
     {
 
-
+        require "application/views/shared/header.php";
+        require "application/views/risorse/index.php";
+        require "application/views/shared/footer.php";
 
     }
-    
+
 }
