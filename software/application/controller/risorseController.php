d7223ac606e71d95a9d9afc7bc2c37a5cb121da2
Matan Davidi <davidi.matan@gmail.com>
2019-10-03 15:46:39 +0200

    Add action 'dettagli' to controller 'Risorse'

--- software/application/controller/risorseController.php
+++ software/application/controller/risorseController.php
@@ -13,4 +13,14 @@ class RisorseController
 
     }
 
+    public function dettagli(string $name)
+    {
+
+        //TODO: Add database logic
+        require "application/views/shared/header.php";
+        require "application/views/risorse/dettagli.php";
+        require "application/views/shared/footer.php";
+
+    }
+
 }
