8f5f0b0f85b018bb2cabac6494947820d354ab6b
Matan Davidi <davidi.matan@gmail.com>
2019-10-04 16:01:18 +0200

    Add required instruction to require fields

--- software/application/libs/js/form-validation/lavori-nuovo.js
+++ software/application/libs/js/form-validation/lavori-nuovo.js
@@ -3,16 +3,16 @@ $(document).ready(() => {
     $("form").validate({
         rules: {
             nome: {
-
+                required: true
             },
             data_inizio: {
-
+                required: true
             },
             data_consegna: {
-
+                required: true
             },
             ore: {
-
+                required: true
             },
             note: {
 
