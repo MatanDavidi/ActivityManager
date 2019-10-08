2872fdb3da26cefc55e04008d5c01b5183517454
Matan Davidi <davidi.matan@gmail.com>
2019-10-08 15:21:38 +0200

    Adjust form validation code to comply with new general select validation class

--- software/application/libs/js/form-validation/assegnazioni-assegna.js
+++ software/application/libs/js/form-validation/assegnazioni-assegna.js
@@ -1,42 +1,22 @@
 $(document).ready(() => {
 
-    //Il valore originale di lavoro
-    const originalLavoroValue = $("#lavoro").val().trim();
-
-    //Le opzioni originali per le risorse
-    const risorsaOptions = $("#risorsa option");
-
-    //Il messaggio di errore da mostrare quando fallisce la validazione del valore all'interno del campo lavoro
-    const LAVORO_ERROR_MESSAGE = "Selezionare un lavoro valido";
-
-    //Il messaggio di errore da mostrare quando fallisce la validazione del valore all'interno del campo risorsa
-    const RISORSA_ERROR_MESSAGE = "Selezionare una risorsa valida";
+    let selectIDs = ["#lavoro", "#risorsa"];
+    let validator = new SelectValidation(selectIDs);
 
     $("form").on("submit", () => {
 
-        //Salvo il select del lavoro e il suo valore per non doverlo cercare due volte
-        let lavoroValue = $("#lavoro").val().trim();
+        //Il valore del lavoro è valido quando è uguale a quello passato dal controller,
+        // quindi non deve essere cambiato dal caricamento al submit
+        let isLavoroValid = validator.isValueSameAsOriginal(selectIDs[0]);
 
         // Variabile booleana che indica se il valore che verrà passato per la registrazione
-        // è uguale a quello inserito dall'applicazione
-        let isLavoroValid = false;
-
-        //Salvo il select delle risorse e il suo valore per non doverlo cercare due volte
-        let risorsaValue = $("#risorsa").val().trim();
-
-        // Variabile booleana che indica se il valore che verrà passato per la registrazione
-        // è contenuto all'interno delle opzioni originali definita dopo il caricamento della pagina
-        let isRisorsaValid;
-
-        //Se non è stato specificato un valore per la risorsa, esso non è valido (ovviamente), dopodiché
-        //controllo se il valore della risorsa che verrà passato è contenuto nelle opzioni originali
-        isRisorsaValid = risorsaValue.length > 0 && optionsContain(risorsaOptions, risorsaValue);
+        // è contenuto all'interno delle opzioni originali definite dopo il caricamento della pagina
+        let isRisorsaValid = validator.isValueInOptions(selectIDs[1]) &&
+            $("#risorsa").val().trim().length > 0;
 
         let lavoroErrorLabel = $("#lavoroSelect label.error");
 
-        //Il valore del lavoro è valido quando è uguale a quello passato dal controller,
-        // quindi non deve essere cambiato dal caricamento al submit
-        if (lavoroValue === originalLavoroValue) {
+        if (isLavoroValid) {
 
             //Se è stato trovato un messaggio d'errore, lo si elimina perché il valore del lavoro è valido
             if (lavoroErrorLabel.length > 0) {
@@ -45,15 +25,14 @@ $(document).ready(() => {
 
             }
 
-            isLavoroValid = true;
-
             //Se il valore non è valido, aggiungi un messaggio di errore
         } else {
 
             //Se non viene trovato un messaggio di errore, aggiungilo
             if (lavoroErrorLabel.length === 0) {
 
-                let errorLabel = $("<label for='lavoro' class='error'>" + LAVORO_ERROR_MESSAGE + "</label>");
+                let lavoroErrorMessage = "Selezionare una lavoro valido";
+                let errorLabel = $("<label for='lavoro' class='error'>" + lavoroErrorMessage + "</label>");
 
                 $("#lavoroSelect").find(".error-container").append(errorLabel);
 
@@ -80,7 +59,8 @@ $(document).ready(() => {
             //Cerca se è presente un messaggio di errore. In caso negativo, aggiungilo
             if (risorsaErrorLabel.length === 0) {
 
-                let errorLabel = $("<label for='risorsa' class='error'>" + RISORSA_ERROR_MESSAGE + "</label>");
+                let risorsaErrorMessage = "Selezionare una risorsa valida";
+                let errorLabel = $("<label for='risorsa' class='error'>" + risorsaErrorMessage + "</label>");
 
                 $("#risorsaSelect").find(".error-container").append(errorLabel);
 
@@ -91,6 +71,86 @@ $(document).ready(() => {
         return isLavoroValid && isRisorsaValid;
 
     });
+
+    // $("form").on("submit", () => {
+    //
+    //     //Salvo il select del lavoro e il suo valore per non doverlo cercare due volte
+    //     let lavoroValue = $("#lavoro").val().trim();
+    //
+    //     // Variabile booleana che indica se il valore che verrà passato per la registrazione
+    //     // è uguale a quello inserito dall'applicazione
+    //     let isLavoroValid = false;
+    //
+    //     //Salvo il select delle risorse e il suo valore per non doverlo cercare due volte
+    //     let risorsaValue = $("#risorsa").val().trim();
+    //
+    //     // Variabile booleana che indica se il valore che verrà passato per la registrazione
+    //     // è contenuto all'interno delle opzioni originali definita dopo il caricamento della pagina
+    //     let isRisorsaValid;
+    //
+    //     //Se non è stato specificato un valore per la risorsa, esso non è valido (ovviamente), dopodiché
+    //     //controllo se il valore della risorsa che verrà passato è contenuto nelle opzioni originali
+    //     isRisorsaValid = risorsaValue.length > 0 && optionsContain(risorsaOptions, risorsaValue);
+    //
+    //     let lavoroErrorLabel = $("#lavoroSelect label.error");
+    //
+    //     //Il valore del lavoro è valido quando è uguale a quello passato dal controller,
+    //     // quindi non deve essere cambiato dal caricamento al submit
+    //     if (lavoroValue === originalLavoroValue) {
+    //
+    //         //Se è stato trovato un messaggio d'errore, lo si elimina perché il valore del lavoro è valido
+    //         if (lavoroErrorLabel.length > 0) {
+    //
+    //             lavoroErrorLabel.remove();
+    //
+    //         }
+    //
+    //         isLavoroValid = true;
+    //
+    //         //Se il valore non è valido, aggiungi un messaggio di errore
+    //     } else {
+    //
+    //         //Se non viene trovato un messaggio di errore, aggiungilo
+    //         if (lavoroErrorLabel.length === 0) {
+    //
+    //             let errorLabel = $("<label for='lavoro' class='error'>" + LAVORO_ERROR_MESSAGE + "</label>");
+    //
+    //             $("#lavoroSelect").find(".error-container").append(errorLabel);
+    //
+    //         }
+    //
+    //     }
+    //
+    //     let risorsaErrorLabel = $("#risorsaSelect label.error");
+    //
+    //     //Indipendentemente dalla validità o meno del valore inserito all'interno del campo Lavoro,
+    //     //se quello inserito nel campo Risorsa è valido, rimuovi gli eventuali messaggi di errore.
+    //     if (isRisorsaValid) {
+    //
+    //         //Se viene trovato un messaggio di errore, rimuovilo
+    //         if (risorsaErrorLabel.length > 0) {
+    //
+    //             risorsaErrorLabel.remove();
+    //
+    //         }
+    //
+    //         //Se invece non dovesse essere valido
+    //     } else {
+    //
+    //         //Cerca se è presente un messaggio di errore. In caso negativo, aggiungilo
+    //         if (risorsaErrorLabel.length === 0) {
+    //
+    //             let errorLabel = $("<label for='risorsa' class='error'>" + RISORSA_ERROR_MESSAGE + "</label>");
+    //
+    //             $("#risorsaSelect").find(".error-container").append(errorLabel);
+    //
+    //         }
+    //
+    //     }
+    //
+    //     return isLavoroValid && isRisorsaValid;
+    //
+    // });
 
     /**
      * Controlla se un valore è contenuto all'interno delle opzioni di un select.
