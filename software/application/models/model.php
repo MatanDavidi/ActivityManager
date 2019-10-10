b1bcd05eafb4413aa92f233134eaa3f10e9a3928
Matan Davidi <davidi.matan@gmail.com>
2019-10-10 15:07:44 +0200

    Add base model class 'Model' members

--- software/application/models/model.php
+++ software/application/models/model.php
@@ -4,4 +4,33 @@
 class Model
 {
 
+    /**
+     * @var string Il nome della tabella da gestire con quest'istanza di Model.
+     */
+    private $tableName;
+
+    /**
+     * @var PDO La connessione al database.
+     */
+    private $database;
+
+    /**
+     * Costruttore per la classe Model
+     * @param $tableName string Il nome della tabella da gestire con quest'istanza di Model.
+     */
+    public function __construct(string $tableName)
+    {
+        $this->tableName = $tableName;
+        $this->database = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";", DB_USERNAME, DB_PASSWORD);
+    }
+
+    public function getAllModels(): Array
+    {
+
+        $statement = $this->database->prepare("SELECT * FROM $this->tableName");
+        return $statement->fetchAll(PDO::FETCH_NUM);
+
+    }
+
+
 }
