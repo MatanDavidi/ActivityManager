185a840a1b5f39179aabb3b69e53861d2e000ef5
Matan Davidi <davidi.matan@gmail.com>
2019-10-10 14:08:04 +0200

    Define database parameters as constants

--- software/application/config/config.php
+++ software/application/config/config.php
@@ -19,5 +19,10 @@ ini_set("display_errors", 1);
  */
 define('URL', 'http://gestione-lavoro.com/');
 
+define("DB_HOST", "127.0.0.1");
+define("DB_PORT", 3306);
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "");
+define("DB_NAME", "gestione_lavoro");
 
 
