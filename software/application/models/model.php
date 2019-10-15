c7d127818e9dbfcd3b64ba952f5d1f465e45450c
Matan Davidi <davidi.matan@gmail.com>
2019-10-15 15:31:25 +0200

    Generalize query condition composition into method 'composePrimaryKeyCondition'

--- software/application/models/model.php
+++ software/application/models/model.php
@@ -34,6 +34,19 @@ class Model
     }
 
     /**
+     * Gets all data from the table whose name corresponds to the value of field 'tableName'.
+     * @return array An associative array containing all data that was read from the database.
+     */
+    protected function getAllModels(): array
+    {
+
+        $statement = $this->database->prepare("SELECT * FROM $this->tableName");
+        $statement->execute();
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    }
+
+    /**
      * Gets a single model identified by its primary key.
      * @param array $keys The table's primary key values for a row.
      * @return array The results of the select query as a bidimensional array containing one or zero rows of the table
@@ -45,14 +58,8 @@ class Model
         //Write the beginning of the query to search in the database
         $query = "SELECT * FROM $this->tableName WHERE ";
 
-        //Compose the query using
-        for ($i = 0; $i < count($keys) - 1; ++$i) {
-
-            $query .= $this->primaryKeyNames[$i] . " = :name$i AND ";
-
-        }
-
-        $query .= $this->primaryKeyNames[count($keys) - 1] . " = :name" . (count($keys) - 1);
+        //Compose the condition of the query
+        $query .= $this->composePrimaryKeyCondition($keys);
 
         //Prepare the query
         $statement = $this->database->prepare($query);
@@ -71,17 +78,55 @@ class Model
     }
 
     /**
-     * Gets all data from the table whose name corresponds to the value of field 'tableName'.
-     * @return array An associative array containing all data that was read from the database.
+     * Deletes a record from the MySQL table whose name is equal to the value of field 'tableName'.
+     * @param array $keys The table's primary key values for a row.
+     * @return bool true if the deletion is successful, false otherwise.
      */
-    protected function getAllModels(): Array
+    public function deleteModel(array $keys): bool
     {
+        //Write the beginning of the query to delete from a table in the database
+        $query = "DELETE FROM $this->tableName WHERE ";
 
-        $statement = $this->database->prepare("SELECT * FROM $this->tableName");
-        $statement->execute();
-        return $statement->fetchAll(PDO::FETCH_ASSOC);
+        //Compose the condition of the query
+        $query .= $this->composePrimaryKeyCondition($keys);
+
+        //Prepare the query
+        $statement = $this->database->prepare($query);
+
+        //Assign to placeholders ':nameN' the value of parameter 'keys'
+        for ($i = 0; $i < count($keys); ++$i) {
+            $statement->bindParam(":key$i", $keys[$i]);
+        }
+
+        //Execute the query
+        return $statement->execute();
 
     }
 
+    /**
+     * Composes the part of a MySQL query that contains the conditions where the primary key is equal to a value.
+     * @param array $keys The names of the fields that make up the primary key.
+     * @return string The condition part of a MySQL query that checks if the primary key is equal to a value.
+     * For example:
+     * composePrimaryKeyCondition(["primary_key_1", "primary_key_2"]);
+     * Will return: "primary_key_1 = :key1 AND primary_key_2 = :key2".
+     */
+    private function composePrimaryKeyCondition(array $keys): string
+    {
+
+        $condition = "";
+
+        //Compose the query using
+        for ($i = 0; $i < count($keys) - 1; ++$i) {
+
+            $condition .= $this->primaryKeyNames[$i] . " = :key$i AND ";
+
+        }
+
+        $condition .= $this->primaryKeyNames[count($keys) - 1] . " = :key" . (count($keys) - 1);
+
+        return $condition;
+
+    }
 
 }
