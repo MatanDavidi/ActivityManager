3a8e32e127a800e9f9700053f43f5687fbb29e08
Matan Davidi <davidi.matan@gmail.com>
2019-10-17 13:36:03 +0200

    Add fields and constructor to class 'Resource'

--- software/application/models/resource.php
+++ software/application/models/resource.php
@@ -4,4 +4,26 @@
 class Resource
 {
 
+    /**
+     * @var string The name of this resource.
+     */
+    private $name;
+
+    /**
+     * @var float The cost per hour of this resource.
+     */
+    private $hourCost;
+
+    /**
+     * Resource constructor.
+     * @param string $name The name of this resource.
+     * @param float $hourCost The cost per hour of this resource.
+     */
+    public function __construct(string $name, float $hourCost)
+    {
+        $this->name = $name;
+        $this->hourCost = $hourCost;
+    }
+
+
 }
