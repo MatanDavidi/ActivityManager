b2d26335682a760779d9e0a4ace879b4b881f778
Matan Davidi <davidi.matan@gmail.com>
2019-10-18 13:53:50 +0200

    Add require statements for classes 'Activity' and 'Resource'

--- software/application/models/assignment.php
+++ software/application/models/assignment.php
@@ -1,5 +1,7 @@
 <?php
 require_once "application/models/model.php";
+require_once "application/models/activity.php";
+require_once "application/models/resource.php";
 
 class Assignment extends Model
 {
