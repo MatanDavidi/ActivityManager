7df4b9d6569efd8e8d110760c3aa13bcbc7ddbf3
Matan Davidi <davidi.matan@gmail.com>
2019-10-18 14:07:23 +0200

    Add function "getAllAssignments"

--- software/application/models/assignment.php
+++ software/application/models/assignment.php
@@ -28,5 +28,40 @@ class Assignment extends Model
         $this->resource = $resource;
     }
 
+    /**
+     * Get all assignments reading their data from the database.
+     * @return array An array containing a object of type Assignment for each line read from the database.
+     */
+    public function getAllAssignments(): array
+    {
+        //Instantiates the array that will be returned.
+        $assignments = [];
+
+        //Get the database's data thanks to superclass 'Model'.
+        $models = $this->getAllModels();
+
+        //Instantiate an empty object of type Activity to use its methods
+        $baseActivity = new Activity();
+
+        //Instantiate an empty object of type Resource to use its methods
+        $baseResource = new Resource();
+
+        // Loop through each element read from the database and for each of them add an object of
+        // type Assignment with the data from the current element from models to array activities.
+        foreach ($models as $model) {
+
+            //Get the activity with the same name as the current model.
+            $modelActivity = $baseActivity->getActivityByName($model["nome_lavoro"]);
+            //Get the resource with the same name as the current model.
+            $modelResource = $baseResource->getResourceByName($model["nome_risorsa"]);
+
+            //Add a new object of type Assignment with the model's data to the array.
+            array_push($assignments, new Assignment($modelActivity, $modelResource));
+
+        }
+
+        return $assignments;
+
+    }
 
 }
