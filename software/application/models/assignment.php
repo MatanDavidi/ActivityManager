118b9efe387a6983b89e75b65ba179bb71fa1101
Matan Davidi <davidi.matan@gmail.com>
2019-10-18 13:51:23 +0200

    Add class fields and constructor function

--- software/application/models/assignment.php
+++ software/application/models/assignment.php
@@ -1,7 +1,30 @@
 <?php
+require_once "application/models/model.php";
 
-
-class Assignment
+class Assignment extends Model
 {
+
+    /**
+     * @var Activity The activity to which the resource is assigned.
+     */
+    private $activity;
+
+    /**
+     * @var Resource The resource assigned to the activity.
+     */
+    private $resource;
+
+    /**
+     * Assignment constructor.
+     * @param Activity $activity The activity to which the resource is assigned.
+     * @param Resource $resource The resource assigned to the activity.
+     */
+    public function __construct(Activity $activity = null, Resource $resource = null)
+    {
+        parent::__construct("assegna", ["nome_lavoro", "nome_risorsa"]);
+        $this->activity = $activity;
+        $this->resource = $resource;
+    }
+
 
 }
