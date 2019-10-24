f5e029b0ed09d279d839c80a346e248988a0f29c
Matan Davidi <davidi.matan@gmail.com>
2019-10-24 15:46:42 +0200

    Add require statements and reference to superclass

--- software/application/models/workHours.php
+++ software/application/models/workHours.php
@@ -1,7 +1,9 @@
 <?php
+require_once "application/models/model.php";
+require_once "application/models/activity.php";
+require_once "application/models/resource.php";
 
-
-class WorkHours
+class WorkHours extends Model
 {
 
 }
