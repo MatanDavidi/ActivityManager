e15ce172db73a38ab07ba0a9b4596b30a71eab92
Matan Davidi <davidi.matan@gmail.com>
2019-11-07 15:30:42 +0100

    Add logic to action "Activities/index"

--- software/application/controller/activitiesController.php
+++ software/application/controller/activitiesController.php
@@ -1,12 +1,30 @@
 <?php
 
 require_once "application/controller/controller.php";
+require_once "application/models/assignment.php";
 
 class ActivitiesController extends Controller
 {
 
+    /**
+     * Shows view "Activities/index", with a list of all activities that the user has permission to see.
+     */
     public function index()
     {
+        if (!(isset($_SESSION["userRole"]) && isset($_SESSION["userRole"]))) {
+            $this->redirect("home");
+        }
+
+        if ($_SESSION["userRole"] == "amministratore") {
+            $baseActivity = new Activity();
+            $activities = $baseActivity->getAllActivities();
+        } else {
+            $baseAssignment = new Assignment();
+            $resource = new Resource();
+            $resource = $resource->getResourceByName($_SESSION["userName"]);
+            $activities = $baseAssignment->getActivitiesAssignedToResource($resource);
+        }
+
         require "application/views/shared/header.php";
         require "application/views/activities/index.php";
         require "application/views/shared/footer.php";
