d825d40e1b0abf2ee11ef67b9236f94ef0474247
Matan Davidi <davidi.matan@gmail.com>
2019-11-12 16:13:05 +0100

    Add logic to get data to fill view "assignments/index" and to add assignment to database

--- software/application/controller/assignmentsController.php
+++ software/application/controller/assignmentsController.php
@@ -6,9 +6,35 @@ require_once "application/models/assignment.php";
 class AssignmentsController extends Controller
 {
 
+    /**
+     * Depending on the request method, either shows the page containing the form to assign a
+     * resource to an activity (GET) or adds a assignment to table 'assegna' of the database (POST).
+     * @param string $activityName The name of the activity to show on the page or to which the resource has to be assigned.
+     */
     public function assign(string $activityName)
     {
+        if (!(isset($_SESSION["userName"]) && isset($_SESSION["userRole"]))) {
+            $this->redirect("home");
+        }
+
+        if ($_SESSION["userRole"] != Resource::ADMINISTRATOR_ROLE) {
+            $this->redirect("activities", "details", [$activityName]);
+        }
+
+        $activityName = urldecode($activityName);
+
+        $baseActivity = new Activity();
+        $baseResource = new Resource();
+
         if ($_SERVER["REQUEST_METHOD"] === "GET") {
+
+            $activity = $baseActivity->getActivityByName($activityName);
+
+            if (is_null($activity)) {
+                $this->redirect("activities");
+            }
+
+            $resources = $baseResource->getAllResources();
 
             require "application/views/shared/header.php";
             require "application/views/assignments/index.php";
@@ -16,8 +42,34 @@ class AssignmentsController extends Controller
 
         } else if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
-            //TODO: Add database logic
-            $this->redirect("activities", "dettagli", Array(0));
+            if (isset($_POST["lavoro"]) && isset($_POST["risorsa"])) {
+
+                if ($_POST["lavoro"] === $activityName) {
+
+                    $resourceName = $this->sanitizeInput($_POST["risorsa"]);
+
+                    $activity = $baseActivity->getActivityByName($activityName);
+                    $resource = $baseResource->getResourceByName($resourceName);
+
+                    if (!is_null($activity) && !is_null($resource)) {
+
+                        $assignment = new Assignment($activity, $resource);
+                        if ($assignment->addAssignment($assignment)) {
+
+                            $this->redirect("activities", "details", [urlencode($activityName)]);
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+            $err_msg = "Impossibile assegnare la risorsa al lavoro";
+            require "application/views/shared/header.php";
+            require "application/views/assignments/index.php";
+            require "application/views/shared/footer.php";
 
         }
 
