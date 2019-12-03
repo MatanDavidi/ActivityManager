8e47c981561928b984e6d112a4b9b743edae88d4
Matan Davidi <davidi.matan@gmail.com>
2019-12-03 14:35:13 +0100

    Avoid use of document.write() and

--- software/application/views/shared/footer.php
+++ software/application/views/shared/footer.php
@@ -28,8 +28,8 @@
                             <p class="footer-text m-0">
                                 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                                 Copyright ©
-                                <script>document.write(new Date().getFullYear());</script>
-                                2019
+                                <span id="year"></span>
+                                <script>$("#year").text(new Date().getFullYear());</script>
                                 All rights reserved | This template is made with <i class="ti-heart"
                                                                                     aria-hidden="true"></i> by <a
                                         href="https://colorlib.com" target="_blank">Colorlib</a>
